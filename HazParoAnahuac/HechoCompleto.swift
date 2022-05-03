d064bf66fd7d2c7b983e6a6326232be6cd903bad
jkashernandeza <jkashernandeza@gmail.com>
2022-05-03 18:11:07 -0500

    Creación de vistas

---
+++ HazParoAnahuac/HechoCompleto.swift
@@ -0,0 +1,20 @@
+//
+//  HechoCompleto.swift
+//  HazParoAnahuac
+//
+//  Created by alumno on 03/05/22.
+//
+
+import SwiftUI
+
+struct HechoCompleto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HechoCompleto_Previews: PreviewProvider {
+    static var previews: some View {
+        HechoCompleto()
+    }
+}
---
+++ HazParoAnahuac/PedidoCompleto.swift
@@ -0,0 +1,20 @@
+//
+//  PedidoCompleto.swift
+//  HazParoAnahuac
+//
+//  Created by alumno on 03/05/22.
+//
+
+import SwiftUI
+
+struct PedidoCompleto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PedidoCompleto_Previews: PreviewProvider {
+    static var previews: some View {
+        PedidoCompleto()
+    }
+}
