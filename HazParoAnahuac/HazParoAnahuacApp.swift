048aadfcb0e75f8f71d703bdc37df8b46c137890
Manuel <manuelam2999@gmail.com>
2022-05-03 17:44:07 -0500

    Initial Commit

---
+++ HazParoAnahuac/HazParoAnahuacApp.swift
@@ -0,0 +1,17 @@
+//
+//  HazParoAnahuacApp.swift
+//  HazParoAnahuac
+//
+//  Created by alumno on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HazParoAnahuacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
