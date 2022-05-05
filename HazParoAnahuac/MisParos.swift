a0b1c9f6867ad7e415e193218cb254defef75d2d
jkashernandeza <jkashernandeza@gmail.com>
2022-05-05 18:50:59 -0500

    misParos

--- HazParoAnahuac/MisParos.swift
+++ HazParoAnahuac/MisParos.swift
@@ -9,19 +9,32 @@ import SwiftUI
 
 struct MisParos: View {
     var body: some View {
-        VStack{
-            Text("Mis Paros")
-                .font(.system(size: 25))
-                .frame(maxWidth: .infinity, alignment: .center)
-            Text("Pedidos")
-                .font(.system(size: 25))
-                .frame(maxWidth: .infinity, alignment: .leading)
-            Button("Pendientes"){
-                PedidoPendiente()
-            }
-            .frame(maxWidth: .infinity, alignment: .center)
-            Spacer()
-        }//VStack
+        NavigationView{
+            VStack{
+                Text("")
+                Text("Pedidos")
+                    .font(.system(size: 30))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                NavigationLink("Pendiente",destination: PedidoPendiente())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                NavigationLink("Completo",destination: PedidoCompleto())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("")
+                Text("Hechos")
+                    .font(.system(size: 30))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                NavigationLink("Pendiente",destination: HechoCompleto())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                NavigationLink("Completo",destination: HechoCompleto())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+            }//VStack
+            .navigationTitle("Mis Paros")
+            .font(.system(size: 20))
+            .navigationBarTitleDisplayMode(.inline)
+        }//NavigationView
     }
 }
 
