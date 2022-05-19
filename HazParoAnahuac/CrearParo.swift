a0c65d84d9946c5505d5cf362fa9cd911cce460e
jkashernandeza <jkashernandeza@gmail.com>
2022-05-18 19:18:35 -0500

    crear paro

--- HazParoAnahuac/CrearParo.swift
+++ HazParoAnahuac/CrearParo.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct CrearParo: View {
+    @State var definicion: String = ""
+    @State var tipo: String = ""
+    @State var precio: String = ""
+    @State var solicitante: String = ""
+    @State var de_donde: String = ""
+    @State var a_donde: String = ""
+    @State var comentario: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("perrito1")
+                .resizable()
+                .frame(width: 200, height: 200)
+            VStack{
+                TextField("Tipo", text: $tipo)
+                TextField("Definición", text: $definicion)
+                TextField("Precio", text: $precio)
+                    .keyboardType(.numberPad)
+                TextField("De dónde", text: $de_donde)
+                TextField("A dónde", text: $a_donde)
+                TextField("Comentario", text: $comentario)
+            }
+            VStack{
+                Button("Crear Paro", action: {})
+            }
+        }
+        .buttonStyle(.bordered)
+        .navigationTitle("Pide Paro")
+        .font(.system(size: 30))
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
